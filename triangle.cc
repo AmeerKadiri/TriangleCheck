d3ec32816848b6558fde11224d4906771ab0ab0f
AmeerKadiri <134822905+AmeerKadiri@users.noreply.github.com>
2023-05-27 22:50:33 -0600

    Update triangle.cc

--- triangle.cc
+++ triangle.cc
@@ -1,31 +1,29 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <fstream>
-#include <cmath>
+#include<iostream>
 using namespace std;
+int main()
+{
+ int a,b,c;
 
-int main(){
+/* Enter all side's Triangle*/
+ cout<<"Enter The Value of a, b, c \n";
+ cin>>a>>b>>c;
 
-double isoscelese, equilateral, right, scalene, nt, both;
-double a, b, c;
-
-cout<<"Enter the Length, Width and Height of a triangle"<<endl;
-cin >> a >> b >> c;
-
-if ((a) )pow(2) * (b) pow(2) = (c) pow(2)){
-  //right
-  if (a = b) {
-    cout<<"Your triangle is a right triangle";
-  else {
-    cout<<"Your triangle is not a right triangle"
-  }
-  }
+ if(a==b && b==c && c==a)
+ {
+  cout<<"The Triangle is Equilateral Triangle\n";
+ }
+ else if(a==b || b==c || c==a)
+ {
+  cout<<"The Triangle is Isosceles Triangle\n";
+ }
+ else if(a*a==b*b+c*c ||b*b==c*c+a*a || c*c==a*a+b*b)
+ {
+  cout<<"The Triangle is Right Triangle\n";
+ }
+ else
+ {
+  cout<<"The Triangle Scalene Triangle\n";
+ }
+ return 0;
 
 }
-
-
-
-
-}
-return0;
