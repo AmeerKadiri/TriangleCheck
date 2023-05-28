c24ceb01fd89972ede9bcca5a24dbe7defb419c2
AmeerKadiri <134822905+AmeerKadiri@users.noreply.github.com>
2023-05-27 22:42:47 -0600

    Add files via upload

---
+++ triangle.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+double isoscelese, equilateral, right, scalene, nt, both;
+double a, b, c;
+
+cout<<"Enter the Length, Width and Height of a triangle"<<endl;
+cin >> a >> b >> c;
+
+if ((a) )pow(2) * (b) pow(2) = (c) pow(2)){
+  //right
+  if (a = b) {
+    cout<<"Your triangle is a right triangle";
+  else {
+    cout<<"Your triangle is not a right triangle"
+  }
+  }
+
+}
+
+
+
+
+}
+return0;
