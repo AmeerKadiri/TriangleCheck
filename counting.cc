0943e68c7f9d7cfaf55c3cc4405e75793675f112
AmeerKadiri <134822905+AmeerKadiri@users.noreply.github.com>
2023-06-12 20:05:04 -0600

    Add files via upload

---
+++ counting.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int ans;
+
+for (int i = 1; i < 5; i++){
+  for (int j = i; j > 0; j--){
+    cout << i << ' ' << j << endl;
+  }
+}
+
+
+   /* Your solution goes here  */
+
+   return 0;
+}
